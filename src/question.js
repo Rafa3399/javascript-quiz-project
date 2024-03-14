a0dc7746a66687ea7bf2ec5a80de914aedb486e1
Rafa <rafaokc33@gmail.com>
2024-03-14 21:35:18 +0100

    Day 1

--- src/question.js
+++ src/question.js
@@ -1,7 +1,18 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
-
-    // 2. shuffleChoices()
+    constructor (text, choices, answer, difficulty){
+        this.text = text; 
+        this.choices = choices; 
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
+    shuffleChoices(){
+        let randomArr = [];
+        for(let i=0 ; i< this.choices.length ; i++){
+            let shuffle = Math.floor(Math.random() * this.choices.length); 
+            randomArr[shuffle] = this.choices[i]
+    }
+    return randomArr;
 }
+}
+let question1 = new Question ('How are you?',['good', 'bad', 'gg'], 'good', 1);
+console.log(question1.shuffleChoices());d
